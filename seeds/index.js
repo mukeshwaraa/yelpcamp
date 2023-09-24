89ddbbb8c77e9a00e3fa6133cf4002fa3c3305d7
Mukeshwaraa k <mukeshwaraa99@gmail.com>
2023-09-24 14:35:54 +0530

    image delete added

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
+const cities1 = require('./cities1')
 const { places, descriptors } = require('./seedHelpers');
 const mongoDB = 'mongodb://127.0.0.1/yelp-camp';
 mongoose.connect(mongoDB, { useNewUrlParser: true });
@@ -9,19 +10,36 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => { console.log("database connected") });
 const Campground = require('../models/campgrounds');
+const { json } = require('express');
 
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 const id = "6509d95f03cb4fdc5b542d73";
+const im =  [
+  {
+    path: 'https://res.cloudinary.com/ds9co9eif/image/upload/v1695544048/YelpCamp/piqjd4qvf1gd5havowvh.jpg',
+    filename: 'YelpCamp/piqjd4qvf1gd5havowvh'
+  },
+  {
+    path: 'https://res.cloudinary.com/ds9co9eif/image/upload/v1695544050/YelpCamp/mzr9w9grxu1ya9sxwcdb.avif',
+    filename: 'YelpCamp/mzr9w9grxu1ya9sxwcdb'
+  },
+  {
+    path: 'https://res.cloudinary.com/ds9co9eif/image/upload/v1695544047/YelpCamp/hrxxm3q26eeb23bu5ioh.avif',
+    filename: 'YelpCamp/hrxxm3q26eeb23bu5ioh'
+  }
+
+  ]
 
 const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
+        const random1000 = Math.floor(Math.random() * 150);
+
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            location: `${cities1[random1000].city}, ${cities1[random1000].state}`,
             name: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: im,
             price: 5,
             description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo, voluptates debitis tempora ut maxime. Odio inventore in vero et dignissimos. Consequuntur quidem beatae eaque dolor maiores atque reiciendis labore ?Inventore est totam obcaecati, non ea rerum illo asperiores? Animi dolor cumque aut quis eos et accusamus est, debitis beatae iusto expedita, quibusdam nam consectetur deserunt perferendis tenetur eveniet hic. Quas inventore impedit deleniti enim porro? At mollitia odio cupiditate dolorem officiis commodi minima pariatur harum, earum vel voluptatum nisi dicta ex.Quo, esse! Alias fuga nam eum labore culpa.",
             author: id,
