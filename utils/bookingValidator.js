cab0e4bc1b5d58a86671a143acb497b27b42ac56
Mukeshwaraa k <mukeshwaraa99@gmail.com>
2023-09-26 14:18:58 +0530

    added sanitization

--- utils/bookingValidator.js
+++ utils/bookingValidator.js
@@ -1,12 +1,7 @@
 const joi = require('joi')
 const AppError = require('./error')
+const {bookingSchema} = require('./schemas.js')
 const bookingValidator = function (req, res, next) {
-  const bookingSchema = joi.object({
-    booking: joi.object({
-      from: joi.date().greater('now').required(),
-      to: joi.date().greater(joi.ref('from')).required()
-    }).required()
-  })
   const { error } = bookingSchema.validate(req.body);
   if (!error) {
     return next()
