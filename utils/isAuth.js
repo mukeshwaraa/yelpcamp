4b4fec171531143ba939738c75a9f3beee262dfb
Mukeshwaraa k <mukeshwaraa99@gmail.com>
2023-09-30 21:58:23 +0530

    multer validations added

--- utils/isAuth.js
+++ utils/isAuth.js
@@ -1,6 +1,5 @@
-const isAuth = async function(req,res,next){
+const isAuthenticated = async function(req,res,next){
     if(!req.isAuthenticated()){
-        console.log(req.body);
         res.cookie('formData',req.body)
         req.flash('error','you need to be logged in')
         res.redirect('/camps/login')
@@ -9,5 +8,14 @@ const isAuth = async function(req,res,next){
     }
 }
 
+const isAuthenticatedd = async function(req,file,cb){
+    console.log('ha')
+    if(!req.isAuthenticated()){
+        console.log(req);
+        return cb(new Error('you need to be logged in'))
+    }else{
+        return cb(null,true)
+    }
+}
 
-module.exports = isAuth;
+module.exports = {isAuthenticated,isAuthenticatedd};
