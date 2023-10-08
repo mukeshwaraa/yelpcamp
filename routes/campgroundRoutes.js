f16e13aa901933a4304642d038047e78f7364abf
mukeshwaraa <104351536+mukeshwaraa@users.noreply.github.com>
2023-10-08 23:54:31 +0530

    Update campgroundRoutes.js

--- routes/campgroundRoutes.js
+++ routes/campgroundRoutes.js
@@ -57,7 +57,7 @@ router.post('/new',cupload,isAuthenticated,campValidator,asyncWrap(async(req,res
     res.redirect(`/camps/${doc._id}`)}) 
 }))
 router.get('/register',(req,res) =>{
-    if(req.get('Referrer') && req.get('Referrer') !== (('/register') && ('/login')) ){
+    if(req.get('Referrer') && !req.get('Referrer').includes(('/register') || ('/login')) ){
    req.session.returnTo = req.get('Referrer') 
     }
     res.render('register');
@@ -85,7 +85,7 @@ router.post('/register',asyncWrap(async(req,res,next) =>{
     }
 }))
 router.get('/login',(req,res) =>{
-    if(req.get('Referrer') && req.get('Referrer') !== (('/register') && ('/login'))){
+    if(req.get('Referrer') && !req.get('Referrer').includes(('/register') || ('/login'))){
    req.session.returnTo = req.get('Referrer')}
     res.render('login');
 })
